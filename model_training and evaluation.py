a3a084067793761dfd21b35cb67bfbd84d322ddd
Manu04Tiwari <marttiwari8219@gmail.com>
2025-03-25 17:19:18 +0530

    4th commit

--- model_training and evaluation.py
+++ model_training and evaluation.py
@@ -2,13 +2,13 @@ from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 import pandas as pd
 
-# Load the dataset (replace 'DataSet-fi.xlsx' with your actual file name)
+# Load the dataset 
 df = pd.read_excel('DataSet-fi.xlsx')
-# One-hot encode categorical features
 
+# One-hot encode categorical features
 df_encoded = pd.get_dummies(df, drop_first=True)
 
-X = df_encoded.drop(columns='Back pain')  # Assuming 'endometriosis' is your target
+X = df_encoded.drop(columns='Back pain')  # Assuming Back-Pain as the target
 y = df_encoded['Back pain']
 scaler = StandardScaler()
 X_scaled = scaler.fit_transform(X)
