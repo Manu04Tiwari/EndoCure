754dbb494bab2a592da45d40bf2e113e9a933e5d
Manu04Tiwari <marttiwari8219@gmail.com>
2025-03-28 19:01:56 +0530

    14th commit

--- main.py
+++ main.py
@@ -1,46 +1,78 @@
-import matplotlib.pyplot as plt
+from data_processing import *
+from sklearn.model_selection import train_test_split, StratifiedKFold, cross_validate
+from sklearn.tree import DecisionTreeClassifier,plot_tree
+from sklearn.preprocessing import StandardScaler
+from sklearn.metrics import make_scorer, precision_score, recall_score, f1_score, accuracy_score, roc_auc_score,confusion_matrix,matthews_corrcoef
+import matplotlib as plt
 import seaborn as sns
-import pandas as pd
+import numpy as np
+import os
 
-def load_data():
-    # Loading DataSet
-    data = pd.read_excel('DataSet-fi.xlsx')
-    return data
+data = pd.read_excel('DataSet-fi.xlsx')
+X = data.drop('Back Pain', axis=1)
+y = data['Back Pain']
 
-def show_histogram(data, column):
-    plt.figure(figsize=(10, 6))
-    sns.histplot(data[column], kde=True)
-    plt.title(f'Histogram of {column}')
-    plt.xlabel(column)
-    plt.ylabel('Frequency')
-    plt.show()
 
-def show_scatter_plot(data, x_column, y_column):
-    plt.figure(figsize=(10, 6))
-    sns.scatterplot(data=data, x=x_column, y=y_column)
-    plt.title(f'Scatter Plot of {x_column} vs {y_column}')
-    plt.xlabel(x_column)
-    plt.ylabel(y_column)
-    plt.show()
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+scaler = StandardScaler()
 
-def show_bar_plot(data, column):
-    plt.figure(figsize=(10, 6))
-    sns.countplot(x=column, data=data)
-    plt.title(f'Bar Plot of {column}')
-    plt.xlabel(column)
-    plt.ylabel('Count')
-    plt.show()
+# Fit and transform the scaler on the training data
+X_train_scaled = scaler.fit_transform(X_train)
+X_test_scaled = scaler.transform(X_test)
 
-def main():
-    print("Welcome to the EndoResearch Final Project!")
-    
-    # Load data
-    data = load_data()
-    
-    # Show different graphs
-    show_histogram(data, 'Pelvic pain')  
-    show_scatter_plot(data, 'Menstrual pain (Dysmenorrhea)', 'Pelvic pain')  
-    show_bar_plot(data, 'Painful cramps during period')  
+# Decision Tree model
+decision_tree_model = DecisionTreeClassifier(random_state=42)
+decision_tree_model.fit(X_train_scaled, y_train)
 
-if __name__ == "__main__":
-    main()
+# Define metrics functions
+scoring = {
+    'Recall': make_scorer(recall_score),
+    'Specificity': make_scorer(recall_score, pos_label=0),  # Assuming 0 is the negative class
+    'Precision': make_scorer(precision_score),
+    'F1-score': make_scorer(f1_score),
+    'Accuracy': make_scorer(accuracy_score),
+    'AUC': make_scorer(roc_auc_score)
+}
+
+# 10-fold cross-validation
+cv = StratifiedKFold(n_splits=10, shuffle=True, random_state=42)
+
+# Calculate cross-validated scores
+cv_results = cross_validate(decision_tree_model, X_train_scaled, y_train, cv=cv, scoring=scoring)
+
+# Display mean and standard deviation for each metric
+for metric in scoring.keys():
+    mean_score = np.mean(cv_results[f'test_{metric}'])
+    std_score = np.std(cv_results[f'test_{metric}'])
+    print(f'{metric} - Mean: {mean_score:.4f}, Std: {std_score:.4f}')
+
+
+# Print most important features 
+feature_importances = decision_tree_model.feature_importances_
+features_df = pd.DataFrame({'Feature': X_train.columns, 'Importance': feature_importances})
+features_df = features_df.sort_values(by='Importance', ascending=False)
+print("Top 5 Most Important Features:")
+print(features_df.head(5)) 
+
+from sklearn.tree import plot_tree
+plt.figure(figsize=(15, 10))
+plot_tree(decision_tree_model, filled=True, feature_names=X_train.columns, class_names=['No Endometriosis', 'Endometriosis'], rounded=True, fontsize=10,max_depth=3)
+plt.tight_layout()
+path = os.path.join('./images produced', 'Decision Tree.png')
+plt.savefig(path)
+
+
+y_pred = decision_tree_model.predict(X_test_scaled)
+conf_matrix = confusion_matrix(y_test, y_pred)
+mcc = matthews_corrcoef(y_test, y_pred)
+print("the mmc is equal to ", mcc)
+# Plot confusion matrix as a heatmap
+plt.figure(figsize=(8, 6))
+sns.heatmap(conf_matrix, annot=True, fmt='d', cmap='Blues', xticklabels=['No Back Pain', 'Back Pain'],
+            yticklabels=['No Back Pain', 'Back Pain'])
+plt.xlabel('Predicted Label')
+plt.ylabel('True Label')
+plt.title('Confusion Matrix')
+path = os.path.join('./images produced', 'confusion matrix.jpeg')
+plt.savefig(path)
+
---
+++ visualising dataset/LogisticRegression.py
@@ -0,0 +1,29 @@
+from sklearn.datasets import load_iris
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score
+import pandas as pd
+
+# Load dataset
+df = pd.read_excel('DataSet-fi.xlsx')
+
+df_encoded = pd.get_dummies(df, drop_first=True)
+
+X = df_encoded.drop(columns='Back pain')  # Assuming 'back pain' is target
+y = df_encoded['Back pain']
+
+# Split the dataset into training and testing sets
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
+
+# Create a logistic regression classifier
+clf = LogisticRegression(max_iter=200)
+
+# Train the classifier
+clf.fit(X_train, y_train)
+
+# Make predictions
+y_pred = clf.predict(X_test)
+
+# Evaluate the classifier
+accuracy = accuracy_score(y_test, y_pred)
+print(f"Accuracy: {accuracy * 100:.2f}%")
